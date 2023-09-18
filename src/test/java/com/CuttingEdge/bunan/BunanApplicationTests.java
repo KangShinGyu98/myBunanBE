ff63bb17acdc5456c0d558c234bdb0f1e91b87a9
Kang Shingyu <rkdtlseb@naver.com>
2023-09-18 18:33:11 +0900

    first commit

---
+++ src/test/java/com/CuttingEdge/bunan/BunanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CuttingEdge.bunan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BunanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
