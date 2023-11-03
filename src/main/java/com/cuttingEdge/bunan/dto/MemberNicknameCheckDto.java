eb01a4c350a9ad8ff9394251a524e8c7a9388418
Shingyu Kang <rkdtlseb@naver.com>
2023-11-03 14:05:23 +0900

    [feature] 회원가입 닉네임 validation 체크

--- src/main/java/com/cuttingEdge/bunan/dto/MemberNicknameCheckDto.java
+++ src/main/java/com/cuttingEdge/bunan/dto/MemberNicknameCheckDto.java
@@ -7,7 +7,7 @@ import javax.validation.constraints.Pattern;
 
 public record MemberNicknameCheckDto(
         @NotBlank(message = "별명은 필수 입력 값입니다.")
-        @Pattern(regexp = "\\S+", message = "별명에 공백을 포함할 수 없습니다.")
+        @Pattern(regexp = "^[가-힣a-zA-Z]{2,10}$", message = "별명은 2~10자의 한글 또는 영어로만 구성되어야 합니다.")
         String nickname){
 
 }
