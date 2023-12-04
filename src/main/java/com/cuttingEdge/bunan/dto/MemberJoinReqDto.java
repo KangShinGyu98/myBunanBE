6b4e1c0fcd66ae7ed19dde754d8a1ba994f0e158
Shingyu Kang <rkdtlseb@naver.com>
2023-12-04 15:23:33 +0900

    [feature] nickname check validation 추가, 배포 전 커밋

--- src/main/java/com/cuttingEdge/bunan/dto/MemberJoinReqDto.java
+++ src/main/java/com/cuttingEdge/bunan/dto/MemberJoinReqDto.java
@@ -4,8 +4,9 @@ package com.cuttingEdge.bunan.dto;
 import javax.validation.constraints.*;
 
 public record MemberJoinReqDto(
+
         @NotBlank(message = "별명은 필수 입력 값입니다.")
-        @Pattern(regexp = "^[가-힣a-zA-Z0-9]{2,12}$", message = "별명은 2~12자의 한글 또는 영어로만 구성되어야 합니다.")
+        @Pattern(regexp = "^[가-힣a-zA-Z0-9]{2,12}$", message = "별명은 2~12자의 한글, 영어, 또는 숫자로만 구성되어야 합니다.")
         String nickname,
         @Email(message = "이메일 형식이 올바르지 않습니다.")
         String email,
--- src/main/java/com/cuttingEdge/bunan/dto/MemberNicknameCheckDto.java
+++ src/main/java/com/cuttingEdge/bunan/dto/MemberNicknameCheckDto.java
@@ -9,7 +9,7 @@ public record MemberNicknameCheckDto(
 
 
         @NotBlank(message = "별명은 필수 입력 값입니다.")
-        @Pattern(regexp = "^[가-힣a-zA-Z]{2,10}$", message = "별명은 2~10자의 한글 또는 영어로만 구성되어야 합니다.")
+        @Pattern(regexp = "^[가-힣a-zA-Z0-9]{2,12}$", message = "별명은 2~12자의 한글, 영어, 또는 숫자로만 구성되어야 합니다.")
         String nickname){
 
 }
