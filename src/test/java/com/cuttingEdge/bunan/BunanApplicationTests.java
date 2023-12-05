1cb591e67a5fa2f3b58beba35ac0aba50d54a54f
Shingyu Kang <rkdtlseb@naver.com>
2023-12-05 22:05:58 +0900

    [feature] migration to mysql

--- src/test/java/com/cuttingEdge/bunan/BunanApplicationTests.java
+++ src/test/java/com/cuttingEdge/bunan/BunanApplicationTests.java
@@ -3,7 +3,7 @@ package com.cuttingEdge.bunan;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class BunanApplicationTests {
 
 	@Test
