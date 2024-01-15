3f58a792a03398322479ea4178e5b60daacd5f0f
Shingyu Kang <rkdtlseb@naver.com>
2024-01-15 19:12:51 +0900

    [feature] cors filter 추가

---
+++ src/main/java/com/cuttingEdge/bunan/config/CorsFilter.java
@@ -0,0 +1,22 @@
+package com.cuttingEdge.bunan.config;
+
+import org.springframework.stereotype.Component;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+public class CorsFilter implements Filter {
+    @Override
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain)
+            throws IOException, ServletException {
+        HttpServletResponse response = (HttpServletResponse) res;
+        response.setHeader("Access-Control-Allow-Origin", "https://www.bunan.co.kr");
+        response.setHeader("Access-Control-Allow-Methods", "GET,POST,OPTIONS,DELETE,PUT");
+        response.setHeader("Access-Control-Allow-Headers", "*");
+        response.setHeader("Access-Control-Allow-Credentials", "true");
+        response.setHeader("Access-Control-Max-Age", "3600");
+        chain.doFilter(req, res);
+    }
+}
